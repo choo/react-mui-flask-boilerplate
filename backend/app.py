f4f3ed61b4e665d5e3bde2be935d715d20f1123f
choo <sugimori.choo@gmail.com>
2019-11-23 06:27:02 +0900

    Change backend not to use CORS library and add arguments setting for execution

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,13 @@
 #! /usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-import os
+import os, argparse
 from flask import Flask, jsonify, send_from_directory, request
-from flask_cors import CORS
 
-
-IS_DEBUG = True
-PORT = 8888
 
 ROOT_DIR = './build'
 STATIC_DIR = os.path.join(ROOT_DIR, 'static')
 app = Flask(__name__, static_folder=STATIC_DIR)
-
-if IS_DEBUG:
-    CORS(app)
 
 
 ''' API definitions '''
@@ -35,4 +28,10 @@ def index(path):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=PORT, debug=IS_DEBUG)
+    parser = argparse.ArgumentParser(description='Flask web application server')
+    parser.add_argument('--port', type=int, dest='port',
+                        required=False, default=8080, help='port number.')
+    parser.add_argument('--is_dev', dest='is_dev', action='store_true',
+                        help='running on dev mode or not')
+    args = parser.parse_args()
+    app.run(host='0.0.0.0', port=args.port, debug=args.is_dev)
