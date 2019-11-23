a7a2dcee105e4d92c296a4b8d64fa62e3caa9d4f
choo <choo.hacks@gmail.com>
2019-11-23 11:17:15 +0900

    Add scripts for running dev server and deployment

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 set -ex
 
-build_frontend () {
-    BASEDIR=$(cd $(dirname $0)/..; pwd)
-    cd $BASEDIR/frontend
-    npm run build
-
-    rm -rf $BASEDIR/backend/build || true # ignore dir not found error
-    mv build $BASEDIR/backend
-}
+# include common.sh
+. ./common.sh
 
 build_frontend
 python3 $BASEDIR/backend/app.py --port 8888
