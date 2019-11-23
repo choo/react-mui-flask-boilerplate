d86e3dc22823a0f91737edd90d5d586a110df8ce
choo <sugimori.choo@gmail.com>
2019-11-23 21:36:52 +0900

    Add necessary npm install command

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,6 +1,7 @@
 build_frontend() {
     BASEDIR=$(cd $(dirname $0)/..; pwd)
     cd $BASEDIR/frontend
+    npm install
     npm run build
 
     rm -rf $BASEDIR/backend/build || true # ignore dir not found error
