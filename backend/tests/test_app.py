a9f5753304889aa7250ba3d9d95695367c129e3a
choo <sugimori.choo@gmail.com>
2019-12-08 06:46:20 +0900

    Modify backend tests

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -14,13 +14,18 @@ class TestApp(TestCase):
     def test_api_data(self):
         res = app.get('/api/sample').get_data(as_text=True)
         data = json.loads(res)
-        self.assertTrue('foo' in data)
+        self.assertEqual(data, [])
+
+        res = app.get('/api/sample?query=a').get_data(as_text=True)
+        data = json.loads(res)
+        self.assertEqual(data[0], 'a')
+        self.assertEqual(data[1], 'aa')
 
     def test_api_param(self):
         PAGE = 1
         res = app.get('/api/sample?page={0}'.format(PAGE)).get_data(as_text=True)
         data = json.loads(res)
-        self.assertEqual(data['page'], PAGE)
+        self.assertEqual(data, [])
 
     def test_index_response(self):
         response = app.get('/')
