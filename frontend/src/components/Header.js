3c067c2ba5cceaf3348e9c19947f6ca1b4616a03
choo <sugimori.choo@gmail.com>
2019-12-08 06:45:55 +0900

    Remove unncessary elements in global nav

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import AppBar     from '@material-ui/core/AppBar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon   from '@material-ui/core/Menu';
-import Button     from '@material-ui/core/Button';
 import Toolbar    from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -11,16 +8,11 @@ const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
     'display' : 'flex',
     'justify-content': 'center',
   },
-  button: {
-  }
 }));
 
 //class Header extends React.Component {
@@ -30,13 +22,9 @@ const Header = (props) => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h5" className={classes.title}>
             {process.env.REACT_APP_WEBSITE_NAME}
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
